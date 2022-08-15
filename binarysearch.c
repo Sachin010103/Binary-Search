e99df6da91f49b6a4c48975f34fd062ec9498b20
Sachin010103 <111006908+Sachin010103@users.noreply.github.com>
2022-08-15 10:30:19 +0530

    Add files via upload

---
+++ binarysearch.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+void takearray(int A[], int size){
+    for (int i = 0; i <size; i++)
+    {
+        printf("Enter the A[%d] element : ",i+1);
+        scanf("%d",&A[i]);
+    }
+   
+}
+
+void printarray(int A[], int lenght){
+    for (int i = 0; i <lenght; i++)
+    {
+         printf("%d ",*(A+i));
+    }
+     printf("\n");
+}
+
+int binarysearch(int A[],int low,int high,int key){
+    int mid=(low+high)/2; 
+    if(low==high && A[low]!=key){
+       return 0;
+    }
+    else{
+    if (A[mid]==key)
+    {
+        return mid;
+    }
+    else if (A[mid]>key)
+    {
+        binarysearch(A,low,mid-1,key);
+    }
+    else if(A[mid]<key){
+       binarysearch(A,mid+1,high,key);
+    }
+    }
+
+}
+
+int main(){
+    int lenght,key;
+    printf("Enter the length of the list : ");
+    scanf("%d",&lenght);
+    int A[lenght];
+    takearray(A,lenght);
+    printarray(A,lenght);
+
+    printf("Enter the element to be search : ");
+    scanf("%d",&key);
+    
+    int index=binarysearch(A,0,lenght-1,key);
+
+   (index==0)? printf("Not found"): printf("The element %d is at index %d ",key ,index+1);
+}
